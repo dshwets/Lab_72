3dfb0dd9e57700248ca709fd08c3cf70e84bda9b
Daniil <dshwets@gmail.com>
2020-10-23 21:15:18 +0600

    Доработанна апишка.

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuoteViewSet
-
+from .views import QuoteViewSet, VoteApiView
 
 router = DefaultRouter()
 router.register('quote', QuoteViewSet, basename='quote')
@@ -11,5 +10,7 @@ app_name = 'api'
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('quote/<int:pk>/like', VoteApiView.as_view(), name='quote_like'),
+    path('quote/<int:pk>/dislike', VoteApiView.as_view(), name='quote_dislike')
 ]
--- source/api/views.py
+++ source/api/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from api.serializers import QuoteCreateSerializer, QuoteUpdateSerializer, QuoteSerializer
-from webapp.models import Quote
+from webapp.models import Quote, Vote
 from .permissions import QuotePermissions
 
 
@@ -20,3 +23,21 @@ class QuoteViewSet(ModelViewSet):
         elif self.request.method == 'PUT':
             return QuoteUpdateSerializer
         return QuoteSerializer
+
+class VoteApiView(APIView):
+    def get(self, request, *args, **kwargs):
+        qoute = get_object_or_404(Quote, pk=kwargs.get('pk'))
+        try:
+            Vote.objects.get(session_key=self.request.session.session_key)
+            return Response({'message': 'you already voted it'}, status=200)
+        except Vote.DoesNotExist:
+            if self.request.path.split('/')[-1] == 'like':
+                Vote.objects.create(session_key=self.request.session.session_key, quote=qoute, rating=1)
+                qoute.rating += 1
+                qoute.save()
+                return Response({'message': 'you like it'}, status=200)
+            else:
+                Vote.objects.create(session_key=self.request.session.session_key, quote=qoute, rating=-1)
+                qoute.rating -= 1
+                qoute.save()
+                return Response({'message': 'you dislike it'}, status=200)
